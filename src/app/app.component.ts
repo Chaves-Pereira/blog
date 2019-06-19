e63f10b584c56db88eea9ca31de22c79f3fee94d
Alexis Chaves Pereira <alexis.chavespereira@moventes.com>
2019-06-19 16:53:56 +0200

    Create blog app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Post } from './post';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'blog';
+  @Input() posts: Post[] = [];
+
+  constructor() {
+    const post1 = new Post(
+      'Premier',
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, sequi. Quo eligendi fuga sit iusto accusantium, porro animi aut excepturi enim est dicta ad laborum officia id iste quidem quod.'
+    );
+    const post2 = new Post(
+      'Deuxième',
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, sequi. Quo eligendi fuga sit iusto accusantium, porro animi aut excepturi enim est dicta ad laborum officia id iste quidem quod.'
+    );
+    const post3 = new Post(
+      'Encore un poste',
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, sequi. Quo eligendi fuga sit iusto accusantium, porro animi aut excepturi enim est dicta ad laborum officia id iste quidem quod.'
+    );
+
+    this.posts.push(post1);
+    this.posts.push(post2);
+    this.posts.push(post3);
+  }
 }
